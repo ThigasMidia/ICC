b290529c92710f8d3c1bebda816fe86cbd02b4b1
THIAGO JOSE BARZOTTO <tjb23@inf.ufpr.br>
2025-03-25 20:19:20 -0300

    atualizacao bissecao

--- teste/ZeroFuncao.c
+++ teste/ZeroFuncao.c
@@ -6,19 +6,61 @@
 #include "ZeroFuncao.h"
 
 // Retorna valor do erro quando método finalizou. Este valor depende de tipoErro
-real_t newtonRaphson (Polinomio p, real_t x0, int criterioParada, int *it, real_t *raiz)
+/*real_t newtonRaphson (Polinomio p, real_t x0, int criterioParada, int *it, real_t *raiz)
 {
 
-}
+}*/
 
 
 // Retorna valor do erro quando método finalizou. Este valor depende de tipoErro
-real_t bisseccao (Polinomio p, real_t a, real_t b, int criterioParada, int *it, real_t *raiz) {
-    *it = 1;
+real_t bisseccao (Polinomio p, real_t a, real_t b, int criterioParada, int *it, real_t *raiz, int Rapido) {
+    int ite = 1;
     real_t xM;
+    real_t *px1, *px2;
+    px1 = malloc(sizeof(real_t));
+    px2 = malloc(sizeof(real_t));
+    *px1 = 0;
+    *px2 = 0;
     xM = (a+b)/2;
-    if()
+    if(Rapido) {
+        calcPolinomio_rapido(p, a, px1, NULL);
+        calcPolinomio_rapido(p, xM, px2, NULL);
+    }
+    else {
+        calcPolinomio_lento(p, a, px1, NULL);
+        calcPolinomio_lento(p, xM, px2, NULL);
+    }
 
+    if((*px1)*(*px2) < 0)
+        b = xM;
+    else if((*px1)*(*px2) > 0)
+        a = xM;
+    else
+        return xM;
+
+    int zero = 1;
+    while((!criterioParada && zero)||(criterioParada == 1 && zero)||(criterioParada == 2 && zero)) {
+        ite++;
+        xM = (a+b)/2;
+        if(Rapido) {
+            calcPolinomio_rapido(p, a, px1, NULL);
+            calcPolinomio_rapido(p, xM, px2, NULL);
+            }
+        else {
+            calcPolinomio_lento(p, a, px1, NULL);
+            calcPolinomio_lento(p, xM, px2, NULL);
+        }
+
+        if((*px1)*(*px2) < 0)
+            b = xM;
+        else if((*px1)*(*px2) > 0)
+            a = xM;
+        if(ite >= 300)
+            zero--;
+    }
+    *it = ite;
+    *raiz = xM;
+    return 0;
 }
 
 
@@ -30,17 +72,17 @@ void calcPolinomio_rapido(Polinomio p, real_t x, real_t *px, real_t *dpx) {
     }
     b = p.p[0] + b*x;
     *px = b;
-    *dpx = c;
+    //*dpx = c;
 }
 
 
 void calcPolinomio_lento(Polinomio p, real_t x, real_t *px, real_t *dpx) {
     *px = 0;
-    *dpx = 0;
+    //*dpx = 0;
 
     for(int i = p.grau; i > 0; i--) {
         *px += p.p[i]*pow(x,i);
-        *dpx += i*p.p[i]*pow(x,i-1);
+        //*dpx += i*p.p[i]*pow(x,i-1);
     }
     *px += p.p[0]*pow(x,0);
 
@@ -60,6 +102,11 @@ int main() {
     calcPolinomio_rapido(*p,0.89,px,dpx);
     printf("POLI: %f\n", *px);
     printf("DERI: %f\n", *dpx);
+    int *px1;
+    px1 = malloc(sizeof(int));
+    bisseccao(*p,0,3,0,px1,dpx,1); 
+    printf("IT: %d\n", *px1);
+    printf("RAIZ: %f\n", *dpx);
 }
 
 
